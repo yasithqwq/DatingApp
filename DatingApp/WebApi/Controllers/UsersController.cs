2066848aa696e7c96283be57fe0e4749450be02e
Yasith Hettige <yasithqwq@gmail.com>
2022-01-25 16:32:20 +0530

    added code

---
+++ DatingApp/WebApi/Controllers/UsersController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Data;
+using WebApi.Entities;
+
+namespace WebApi.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+            _context = context;
+        }
+        
+        [AllowAnonymous]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _context.Users.ToListAsync();
+
+            return users;
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var users = await _context.Users.Where(u => u.Id == id).FirstOrDefaultAsync();
+
+            return users;
+        }
+    }
+}
---
+++ DatingApp/WebApi/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApi.Entities;
+
+namespace WebApi.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options):base(options)
+        {
+        }
+        public DbSet<AppUser> Users { get; set; }
+    }
+}
